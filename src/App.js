aa78f30b26280f8dd41ac30db1a9ea8d9ad2e208
Jehoon <limjhoon8@gmail.com>
2024-07-14 19:56:48 +0900

    # React

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './config/route-config';
 
-
-
 const App = () => {
   
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
-  console.log('home!');
 
   return (
     <>
-    
       <Outlet />
-      
     </>
   );
 };
